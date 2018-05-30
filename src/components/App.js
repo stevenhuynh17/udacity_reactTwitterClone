d98109dd6360a0aaa526079ce663a1fd17d6e177
Steven <stevenhuynh17@gmail.com>
2018-05-30 15:08:53 -0700

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { handleInitialData } from '../actions/shared'
+import Dashboard from './dashboard'
+import LoadingBar from 'react-redux-loading'
+
+class App extends Component {
+  componentDidMount() {
+    this.props.dispatch(handleInitialData())
+  }
+  render() {
+    return (
+      <div>
+        <LoadingBar />
+        {this.props.loading === true
+          ? null
+          : <Dashboard/>
+        }
+      </div>
+    )
+  }
+}
+
+function mapStateToProps({ authedUser }) {
+  return {
+    loading: authedUser === null
+  }
+}
+
+export default connect(mapStateToProps)(App)
